88c57a3b79e54bb6190e8f2fdc991a8740413b98
Hovnos <filipsimon5337@gmail.com>
2023-10-03 23:18:21 +0200

    j

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "rfmzpuww", // Your ElephantSQL username
-  password: "3TE30jD_m36jCPKgJoMzD9gG_sOTvt1r", // Your ElephantSQL password
-  host: "cornelius.db.elephantsql.com", // Your ElephantSQL host
-  port: 5432, // Default PostgreSQL port
-  database: "rfmzpuww", // Your default database
+  user: "rfmzpuww",
+  password: "3TE30jD_m36jCPKgJoMzD9gG_sOTvt1r",
+  host: "cornelius.db.elephantsql.com",
+  port: 5432,
+  database: "rfmzpuww",
 });
 
 module.exports = pool;
